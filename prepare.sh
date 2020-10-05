45e109cf36788feac7ceb05f260b3fb92fc4b24f
Ceri Hughes <github@cerihughes.co.uk>
2020-10-05 12:08:41 +0100

    Change name to openapi and update pubspecs

--- prepare.sh
+++ prepare.sh
@@ -1,2 +1,2 @@
 export DART_POST_PROCESS_FILE="/usr/local/bin/dartfmt -w"
-openapi-generator generate -i mgm-schema/v1/openapi.yaml -o oas -g dart
+openapi-generator generate -i mgm-schema/v1/openapi.yaml -o openapi -g dart
